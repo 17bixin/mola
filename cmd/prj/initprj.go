1589f4f904673573da62956d6d8c0587426017cb
timeloveboy <734991033@qq.com>
2018-01-24 12:46:00 +0800

    ~

--- cmd/prj/initprj.go
+++ cmd/prj/initprj.go
@@ -1,19 +1,29 @@
 package prj
 
 import (
+	"fmt"
 	"github.com/17bixin/gobase/log"
 	"os"
 	"os/exec"
 )
 
 func Initprj(name, path string, servicetype int) {
-	if os.IsExist(path + "/" + name) {
+	targetdir := path + "/" + name
+	if os.IsExist(targetdir) {
 		log.Error(log.Fields{
 			"err":           "target folder already exist",
-			"target folder": path + "/" + name,
+			"target folder": targetdir,
 		})
 		return
 	}
-	os.Mkdir(path+"/"+name, os.ModePerm)
-	exec.Command("git", "clone ")
+	os.Mkdir(targetdir, os.ModePerm)
+	os.Mkdir(targetdir+"/src", os.ModePerm)
+	os.Mkdir(targetdir+"/bin", os.ModePerm)
+	os.Setenv("GOPATH", targetdir)
+	goget := exec.Command("go", "get", "github.com/17bixin/gobase")
+	out, err := goget.CombinedOutput()
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(string(out))
 }
--- cmd/prj/initprj_test.go
+++ cmd/prj/initprj_test.go
@@ -1,1 +1,7 @@
 package prj
+
+import "testing"
+
+func TestInitprj(t *testing.T) {
+	Initprj("mymola", "/CODE/github.com/17bixin/", 3)
+}
